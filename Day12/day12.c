9a004f862634d631df9264ff5c44a6e5476ae940
Varista Gupta <varistagupta07@gmail.com>
2026-07-08 12:59:56 +0530

    DAY 12

---
+++ Day12/day12.c
@@ -0,0 +1,94 @@
+// Problem: Write a program to check whether a given matrix is symmetric. A matrix is said to be symmetric if it is a square matrix and is equal to its transpose (i.e., element at position [i][j] is equal to element at position [j][i] for all valid i and j).
+
+// Input:
+// - First line: two integers m and n representing the number of rows and columns
+// - Next m lines: n integers each representing the elements of the matrix
+
+// Output:
+// - Print "Symmetric Matrix" if the given matrix is symmetric
+// - Otherwise, print "Not a Symmetric Matrix"
+// Example:
+// Input:
+// 3 3
+// 1 2 3
+// 2 4 5
+// 3 5 6
+// Output:
+// Symmetric Matrix
+// Explanation:
+// The matrix is square (3 × 3) and for every i and j, element[i][j] = element[j][i].
+// Test Cases:
+// Test Case 1:
+// Input:
+// 2 2
+// 1 2
+// 2 1
+// Output:
+// Symmetric Matrix
+// Test Case 2:
+// Input:
+// 3 3
+// 1 0 1
+// 2 3 4
+// 1 4 5
+// Output:
+// Not a Symmetric Matrix
+// Test Case 3:
+// Input:
+// 2 3
+// 1 2 3
+// 4 5 6
+// Output:
+// Not a Symmetric Matrix
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int m, n, i, j;
+    scanf("%d %d", &m, &n);
+    int **arr = (int **)malloc(m * sizeof(int *));
+    for (i = 0; i < m; i++)
+    {
+        arr[i] = (int *)malloc(n * sizeof(int));
+    }
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    int flag = 1;
+    if (m != n)
+    {
+        flag = 0;
+    }
+    else
+    {
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (arr[i][j] != arr[j][i])
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                break;
+            }
+        }
+    }
+    if (flag == 1)
+    {
+        printf("Symmetric Matrix\n");
+    }
+    else
+    {
+        printf("Not a Symmetric Matrix\n");
+    }
+    return 0;
+}
