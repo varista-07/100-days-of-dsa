a4828ac202ec5ac967fce203a9aed4b4aca810f0
Varista Gupta <varistagupta07@gmail.com>
2026-07-09 13:53:16 +0530

    DAY 10

--- Day_10/day10.c
+++ Day_10/day10.c
@@ -1,19 +1,15 @@
 // Problem: Read a string and check if it is a palindrome using two-pointer comparison.
-
 // Input:
 // - Single line: string s
-
 // Output:
 // - Print YES if palindrome, otherwise NO
-
 // Example:
 // Input:
 // level
-
 // Output:
 // YES
-
 // Explanation: String reads same forwards and backwards
+
 
 #include <stdio.h>
 #include <string.h>
